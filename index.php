46fc947b58319178b2d03e49016d872202ee719f
richtwin567 <anakai.richards@gmail.com>
2020-11-23 00:20:57 -0500

    Rearranged project structure to folder-by-feature rather than folder-by-type.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <link rel='stylesheet' href="common/styles/global.css">
+    <!--<script src="" defer></script>-->
+</head>
+
+<body>
+    <!--include-->
+    <?php include("common/templates/header.html") ?>
+    <main>
+        <!-- Dynamic Content goes here-->
+        <div id='hero'>
+            <div id="hero-call-to-action">
+                <h1>WELCOME</h1>
+                <p>Order now to experience satisfaction beyond compare</p>
+                <a href='menu.html' class='btn btn-regular'>Shop Now</a>
+            </div>
+        </div>
+    </main>
+    <?php include("common/templates/footer.html") ?>
+    
+</body>
+
+</html>
