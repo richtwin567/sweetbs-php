c1a4326e5d248fc7128a4f36014ae3fbfa12e575
richtwin567 <anakai.richards@gmail.com>
2020-11-24 21:15:59 -0500

    installed mongodb for nodejs

--- presentation/website/menu.php
+++ presentation/website/menu.php
@@ -7,7 +7,7 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="styles/menu.css">
     <link rel='stylesheet' href="../styles/global.css">
 </head>
 
@@ -15,6 +15,8 @@
     <?php include("../templates/header.php")?>
         <main>
             <!-- Dynamic Content goes here-->
+            <section id="filter"></section>
+            <section id="menu"></section>
         </main>
        <?php include("../templates/footer.php") ?>
 </body>
