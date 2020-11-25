8d751f35e43e515172eed732fa6cea9ecb05c64a
Jason <jasonblue1626@gmail.com>
2020-11-24 21:23:10 -0500

    Added class files and hashing for persistence layer

---
+++ persistence/db/db.js
@@ -0,0 +1,30 @@
+// Import Mongo Client
+const MongoClient = require('mongodb').MongoClient;
+
+
+async function connect(){
+    // Initialize URI and Mongo Client
+    const uri = "mongodb+srv://admin_COMP2140:W62aZqXfeH4RrYkd@comp2140-project-2020-c.yvx5q.mongodb.net/sweebs?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true});
+
+    // Try-catch block to handle connecting to the database
+    try{
+        console.log('Connecting');
+        await client.connect();
+        console.log('Connected');
+        await listDatabases(client);
+    }catch(err){
+        console.error(err);
+    }finally{
+        await client.close();
+        console.log('Closed Connection');
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+connect().catch(console.error);
