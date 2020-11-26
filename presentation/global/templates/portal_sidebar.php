06fbf15d6fa058b06de97d898a4845bffff2ab3c
Jason <jasonblue1626@gmail.com>
2020-11-26 18:51:15 -0500

    Added new CSS files

---
+++ presentation/global/templates/portal_sidebar.php
@@ -0,0 +1,26 @@
+<div class='sidebar-wrapper'>
+    <div class='sidebar'>
+        <ul>
+            <li><a href='#'>
+                <div id='dashboard-icon'></div>
+                Dashboard
+                </a>
+            </li>
+            <li><a href='#'>
+                <div id='orders-icon'></div>
+                Orders
+                </a>
+            </li>
+            <li><a href='#'>
+                <div id='shopping-list-icon'></div>
+                Shopping List
+                </a>
+            </li>
+            <li><a href='#'>
+                <div id='menu-manager-icon'></div>
+                Menu Manager
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
