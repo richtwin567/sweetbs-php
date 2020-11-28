6ea0ef71c38746fa6d88585247e6bc100c4deba3
richtwin567 <anakai.richards@gmail.com>
2020-11-28 16:59:23 -0500

    added titles to the shooping cart and menu pages

--- presentation/website/menu.php
+++ presentation/website/menu.php
@@ -43,7 +43,7 @@ try {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Menu</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="styles/menu.css">
