6024b1444485c634f3f2f33a96ad1a80b85d048f
richtwin567 <anakai.richards@gmail.com>
2020-11-30 14:32:24 -0500

    abotu page layout complete

--- presentation/website/about.php
+++ presentation/website/about.php
@@ -14,12 +14,20 @@ $session = new Session();
     <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel='stylesheet' href="../global/styles/global.css">
+    <link rel="stylesheet" href="styles/about.css">
 </head>
 
 
 <body>
-    <?php include("../global/templates/header.php");?>
-    <main><div id="story-logo"><img src="../global/client/Logo no bg.png" alt="sweetb's logo"></div></main>
-    <?php include("../global/templates/footer.php");?>
+    <?php include("../global/templates/header.php"); ?>
+    <main>
+        <div id="story-logo"><img src="../global/client/Logo no bg.png" alt="sweetb's logo"></div>
+        <div id="story-details">
+            <h1 id="page-title">Our Story</h1>
+            <p></p>
+        </div>
+    </main>
+    <?php include("../global/templates/footer.php"); ?>
 </body>
+
 </html>
