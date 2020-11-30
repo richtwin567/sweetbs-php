ffb0a5c08d1b1022e244e53164c21c706ff0a91c
Jason <jasonblue1626@gmail.com>
2020-11-30 04:23:04 -0500

    Completed some of the css for the order collator and asynchronous calls to mongo

--- business_logic/order_collator/order_collator.js
+++ business_logic/order_collator/order_collator.js
@@ -1,5 +1,29 @@
 // Imports
 
-function fetchOrders(){
-    
+import { OrderDocuments } from '../../persistence/db/documents.js';
+
+window.addEventListener('load', function(){
+    console.log('I work');
+    updateCollator();
+    // Refresh the data for the collator every two minutes
+    setInterval(updateCollator, 120000);
+});  
+
+
+async function updateCollator(){
+    let orderCount = document.getElementById('order-count');
+    let ordersArr = await fetchOrders();
+    // Update the number of orders
+    orderCount.innerHTML = ordersArr.length;
+}
+
+async function fetchOrders(){
+    let response = await fetch('http://127.0.0.1:3000/orders');
+    if (response.ok){
+        return response.json();
+    // If any unexpected errors happen while fetching, an error is thrown
+    }else{
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    }
 }
