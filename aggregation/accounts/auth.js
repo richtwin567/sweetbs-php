7561b61d5a995d619726c708e2f8d2e434ae5065
AliAli112 <alioren112@outlook.com>
2020-12-01 05:02:06 -0600

    About the start auth process

--- aggregation/accounts/auth.js
+++ aggregation/accounts/auth.js
@@ -1,1 +1,12 @@
-// This module primarily handles the authentication process
+import {User} from '../data_classes/user.js';
+//This doesnt nothing yet
+async function authUsers(queryString) {
+	return await fetch("http://127.0.0.1:3000/users"+queryString, {
+		method: "GET",
+		headers: {
+			"Content-Type": "Application/json",
+		},
+    })
+    
+
+    }
