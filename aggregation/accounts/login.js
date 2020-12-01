f689c43baf1f9034a40fe9bb6dafa6059212e87a
AliAli112 <alioren112@outlook.com>
2020-12-01 01:25:33 -0600

    Card details first name last name text field added, customer object created successfully

--- aggregation/accounts/login.js
+++ aggregation/accounts/login.js
@@ -1,16 +1,24 @@
-import { Customer } from '../data_classes/user.js';
+import { Customer, RealName } from '../data_classes/user.js';
 
 var username = document.getElementById("username")
 var email = document.getElementById("email")
 var password = document.getElementById("password")
-var name = document.getElementById("name")
+var fname = document.getElementById("fname")
+var lname = document.getElementById("lname")
+var card = document.getElementById("card")
 var address = document.getElementById("address")
 var form = document.getElementById("form")
 
+function createRegisteredCustomer(username, email, password, card, fname, lname, address){
+    let myRealname = new RealName(fname.value, lname.value)
+    var ResgisteredCustomer = new Customer(username.value, email.value, password.value, card.value, myRealname, address.value)
+    console.log(ResgisteredCustomer.getCard());
+}
+
 form.addEventListener("submit", (event) =>{
     console.log("here");
-    var ResgisteredCustomer = new Customer(username.value, email.value, password.value, "0000", name.value, address.value)
-    console.log(ResgisteredCustomer);
+    createRegisteredCustomer(username, email, password, card, fname, lname, address);
+    window.location.replace('http://localhost:8080/presentation/website/login.php')
     event.preventDefault();
 })
 //var ResgisteredCostumer = new Customer(username.value, email.value, password.value, name.value, address.value)
--- presentation/website/register.php
+++ presentation/website/register.php
@@ -17,6 +17,7 @@ $session = new Session();
     <link rel='stylesheet' href="../global/styles/global.css">
     <script src="../../aggregation/accounts/login.js" type="module"></script>
     <script src="../../aggregation/data_classes/user.js" type="module"></script>
+    <script src="../../persistence/db/documents.js" type="module"></script>
 </head>
 
 <body>
@@ -32,8 +33,12 @@ $session = new Session();
                 <input type="email" id="email" name="email" required aria-required="true"><br />
                 <label>Password</label><br />
                 <input type="password" id="password" name="password" required aria-required="true"></br />
-                <label>Name</label><br />
-                <input type="text" id="name" name="name" required aria-required="true"></br />
+                <label>First Name</label><br />
+                <input type="text" id="fname" name="fname" required aria-required="true"></br />
+                <label>Last Name</label><br />
+                <input type="text" id="lname" name="lname" required aria-required="true"></br />
+                <label>Card Details</label><br />
+                <input type="text" id="card" name="card" required aria-required="true"></br />
                 <label>Default Delivery Address</label><br />
                 <input type="text" id="address" name="address" required aria-required="true"></br />
                 <input type="submit" id="submit" value="submit">
