8f86b573c449e8b4d456f67c929666c66d879731
richtwin567 <anakai.richards@gmail.com>
2020-12-01 15:00:20 -0500

    route change again

--- persistence/server.js
+++ persistence/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = 'sweetbs.herokuapp.com';
-const port = 3000;
+const port = 443;
 const server = require('./route.js'); // imports the routing file
 server.listen(port, hostname, () => {
   console.log(`Server running at https://${hostname}:${port}/`);
