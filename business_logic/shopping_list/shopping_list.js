dfd20a5a1de7976200551ab87d39431a2929ae93
Teriq Strachan <teriqstrachan@yahoo.com>
2020-12-01 12:55:43 -0500

    Additional javascript functionality

--- business_logic/shopping_list/shopping_list.js
+++ business_logic/shopping_list/shopping_list.js
@@ -1,23 +1,27 @@
 window.onload = function(){
   var innercont = document.querySelector("#innercont");
   var button = document.querySelector("#genbtn");
-    var rows = document.getElementsByClassName("shoplist-row");
+  var rows = Array.from(document.getElementsByClassName("shoplist-row"));
   var ingList = ["Cream","Cream Cheese", "Butter", "Sour Cream",
   "Sugar","Egg","Graham Cracker","Cherries","Oreos","Rum Cream","Guava", "Flour",
 "Cinnamon","Baking Powder","Salt","Oats","Vanilla"];
+  var count = 0;
 
+  console.log("Working...");
   button.addEventListener("click",addElement);
 
   function addElement(){
     ingList.forEach(sendImg);
+    deleteItem();
   }
 
   function sendImg(pic){
     if(rows.length%2 == 0){
       var div = document.createElement("div");
       div.classList.add("shoplist-row");
+      div.setAttribute("id",count);
       var img = document.createElement("img");
-      img.setAttribute('src','../../presentation/images/'+pic+'.png');
+      img.setAttribute('src','../../presentation/global/images/ingredients_images/'+pic+'.png');
       var p = document.createElement("p");
       p.innerHTML = pic;
       var div2 = document.createElement("div");
@@ -26,13 +30,14 @@ window.onload = function(){
       div.appendChild(p);
       div.appendChild(div2);
       innercont.appendChild(div);
-      console.log(img+".jpg");
+      count++;
     } else{
       var div = document.createElement("div");
-      div.classList.add("shoplist-row");
       div.classList.add("even");
+      div.classList.add("shoplist-row");
+      div.setAttribute("id",count);
       var img = document.createElement("img");
-      img.setAttribute('src','../../presentation/images/'+pic+'.png');
+      img.setAttribute('src','../../presentation/global/images/ingredients_images/'+pic+'.png');
       var p = document.createElement("p");
       p.innerHTML = pic;
       var div2 = document.createElement("div");
@@ -41,8 +46,23 @@ window.onload = function(){
       div.appendChild(p);
       div.appendChild(div2);
       innercont.appendChild(div);
-      console.log(pic+".jpg");
+      div.addEventListener("click", function(){
+
+      })
+      count++;
     }
   }
+
+
+  function deleteItem(){
+    var items = document.querySelectorAll(".shoplist-row");
+    console.log(items);
+    items.forEach( element=> {
+        element.addEventListener("click",()=>innercont.removeChild(element))
+    })
+  }
+
+
+
 
 }
