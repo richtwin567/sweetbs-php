e3f5926a4f9386db42d5b3a88fed7ffcf09836df
richtwin567 <anakai.richards@gmail.com>
2020-12-01 17:07:20 -0500

    no address for listening, only port

--- persistence/server.js
+++ persistence/server.js
@@ -2,6 +2,4 @@ const http = require('http');
 const hostname = 'sweetbs-backend.herokuapp.com';
 const port = process.env.PORT || 8080;
 const server = require('./route.js'); // imports the routing file
-server.listen(port, hostname, () => {
-  console.log(`Server running at https://${hostname}:${port}/`);
-});
+server.listen(port);
