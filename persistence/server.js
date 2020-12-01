70579f3a200efa46e5cb6aeb1c225116a4bdc359
richtwin567 <anakai.richards@gmail.com>
2020-12-01 16:55:38 -0500

    change port

--- persistence/server.js
+++ persistence/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = 'sweetbs-backend.herokuapp.com';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 const server = require('./route.js'); // imports the routing file
 server.listen(port, hostname, () => {
   console.log(`Server running at https://${hostname}:${port}/`);
