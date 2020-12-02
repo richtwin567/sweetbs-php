192b5a4fe91ef16ad514978c2a1e562698189217
Jason <jasonblue1626@gmail.com>
2020-12-01 20:04:19 -0500

    Completed 90% of collator functionality

--- business_logic/order_collator/order_collator.js
+++ business_logic/order_collator/order_collator.js
@@ -1,125 +1,192 @@
 // Imports
-window.addEventListener('load', function(){
+import { MenuItem } from '../../aggregation/data_classes/menu_item.js';
+import { OrderItem } from '../../aggregation/data_classes/order_item.js';
+
+window.addEventListener('load', function () {
     console.log('I work');
     // Refresh the data for the collator every minute
-    updateCollator()
-    setInterval(updateCollator, 60000);
-});  
-
-
-// This function exists solely to be exported to the dashboard
-function getOrdersAmount(ordersARr){
-
-}
+    setInterval(updateCollator(), 60000);
+});
 
 /**
  * The main driver for the order collator. 
  * This function is run every minute to refresh the order collator with new orders
  */
-async function updateCollator(){
-    checkOffItem();
-    displayCollapsibleOrder();
+async function updateCollator() {
+    processCollapsibleOrders();
+
+    // Initializing key variables
     let orderCount = document.getElementById('order-count');
     let ordersArr = await fetchOrders();
+    let menuItems = await fetchMenuItems();
 
     // Update the number of orders
     orderCount.innerHTML = ordersArr.length;
 
     // Display basic order
     let orders = document.getElementById('new-orders');
-    // orders.innerHTML = '';
-    for(let index =0; index < ordersArr.length; index++){
-        displayOrder(orders, index, ordersArr[index].customer, null, 0);
+    orders.innerHTML = '';
+    for (let index = 0; index < ordersArr.length; index++) {
+        // Create the array of order items to be displayed
+        let orderItems = createOrderItemArray(ordersArr[index].items, menuItems);
+
+        // Display the orders
+        displayOrder(orders, index, ordersArr[index].customer, orderItems);
     }
 }
 
-async function fetchMenuItem(itemID){
-    let response = await fetch(`http://127.0.0.1:3000/?_id=${itemID}`);
-    if (response.ok){
-        return response.json();
-    }else{
-        const message = `An error has occured: ${response.status}`;
-        throw new Error(message);
+function createOrderItemArray(orderItems, menuItems) {
+    let orderItemArr = []
+    for (let i = 0; i < orderItems.length; i++) {
+        let menuItem = menuItemIdSearch(menuItems, orderItems[i].menuitem)
+        if (menuItem !== null) {
+            let orderItem = new OrderItem(menuItem, orderItems[i].qty);
+            orderItemArr.push(orderItem);
+        }
     }
+    return orderItemArr;
 }
 
-function parseMenuItemData(menuItem){
-
+function menuItemIdSearch(menuItems, desiredId) {
+    for (const menuItem of menuItems) {
+        if (menuItem.getId() === desiredId) {
+            return menuItem;
+        }
+    }
+    return null;
 }
 
-async function fetchOrders(){
+async function fetchMenuItems() {
+    return await fetch("http://127.0.0.1:3000/menuitems", {
+        method: "GET",
+        headers: {
+            "Content-Type": "Application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            var menulist = [];
+            for (var el of data) {
+                menulist.push(
+                    new MenuItem(
+                        el["_id"],
+                        el["name"],
+                        el["description"],
+                        el["price"],
+                        el["picturelink"],
+                        el["ingredient_ids"]
+                    )
+                );
+            }
+            return menulist;
+        })
+        .catch((err) => console.log(err));
+}
+
+async function fetchOrders() {
     let response = await fetch('http://127.0.0.1:3000/orders');
-    if (response.ok){
+    if (response.ok) {
         return response.json();
-    // If any unexpected errors happen while fetching, an error is thrown
-    }else{
+        // If any unexpected errors happen while fetching, an error is thrown
+    } else {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
     }
 }
+
+
+
+/**
+ * Displays the collapsible orders using event delegation
+ */
+function processCollapsibleOrders() {
+    let newOrdersDiv = document.getElementById('new-orders');
+    newOrdersDiv.addEventListener('click', function (event) {
+        let element = event.target;
+        // Traverse up the parent nodes
+        let orderOverview = element.parentNode.parentNode.parentNode.nextElementSibling;
+        if (element && element.classList.contains('show-hide')) {
+            animateCollapsibleArrow(element);
+            displayCollapsibleOrder(orderOverview);
+        } else if (element.type === 'checkbox' && element.classList.contains('item-check')) {
+
+            // Selecting the ul for the collapsible details
+            let collapsibleDetails = element.parentNode.parentNode;
+            let checkTick = collapsibleDetails.children[4].children[0];
+            checkOffItem(element, checkTick);
+
+        }else if (element.type == 'checkbox' && element.classList.contains('check-off')){
+            let checkOffTick = element.parentNode.parentNode.nextElementSibling.children[0].children[0];
+            checkOffOrder(element, checkOffTick);
+        }
+
+    });
+}
+
+function animateCollapsibleArrow(element) {
+    if (element.classList.contains('expanded')) {
+        element.classList.remove('expanded');
+        element.classList.add('unexpanded');
+    } else {
+        element.classList.remove('unexpanded');
+        element.classList.add('expanded');
+    }
+}
+
+
+function displayCollapsibleOrder(element) {
+    if (element.style.maxHeight) {
+        element.style.maxHeight = null;
+    } else {
+        element.style.maxHeight = element.scrollHeight + "px"
+    }
+}
+
 
 /**
  * Checks off a menu item on the collator
  * @param {Number} itemNum The item number of the item being checked off.
  * @returns {Boolean} True if the item has been checked, and false if not
  */
-function checkOffItem(){
-    let checkOffItem = document.getElementsByClassName(`item-check`);
-    let checkOffTick = document.getElementsByClassName(`order-tick`);
-    for(let i=0; i < checkOffItem.length; i++){
-        checkOffItem[i].addEventListener('change', function(){
-            if(this.checked){
-                checkOffTick[i].src = '../global/icons/done-24px.svg';
-                return true;
-            }else{
-                checkOffTick[i].src = '../global/icons/grayed_done_check.png';
-                return false;
-            }
-        });
+function checkOffItem(checkbox, tick) {
+    console.log('Im here');
+    if (checkbox.checked) {
+        tick.src = '../global/icons/done-24px.svg';
+        return true;
+    } else {
+        tick.src = '../global/icons/grayed_done_check.png';
+        return false;
     }
 }
 
-async function displayCollapsibleOrder(){
-    let showHide = document.getElementsByClassName('show-hide');
-    let orderOverview = document.getElementsByClassName('order-grid-wrap');
-    for(let i=0; i < showHide.length; i++){
-        
-        showHide[i].addEventListener('click', function (){
-            // Rotate icon
-            if(showHide[i].classList.contains('expanded')){
-                showHide[i].classList.remove('expanded');
-                showHide[i].classList.add('unexpanded');
-            }else{
-                showHide[i].classList.remove('unexpanded');
-                showHide[i].classList.add('expanded');  
-            }
-            let content = orderOverview[i].nextElementSibling;
-            console.log(content);
-            console.log(content.style.maxHeight);
-           // console.log(content);
-            if(content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px"
-            }
-        });
+function checkOffOrder(checkbox, tick){
+    if(checkbox.checked){
+        tick.src = '../global/icons/done_all-black-48dp.svg'
+    }else{
+        tick.src = '../global/icons/grayed_checkmark.png'
     }
 }
 
-function createOrderBreakdown(menuItem, quantity){
-    let orderBreakdownHTML= `
-    <ul class='collapsible-details'>
-        <li><input type='checkbox' class='item-check'></li>
-        <li id='pastry'><img src='../global/images/cheesecake2.jpg'></li>
-        <li>${menuItem.getName()}</li>
-        <li>${quantity}</li>
-        <li><img src='../global/icons/grayed_done_check.png' class='order-tick'></li>
-    </ul>`
+
+function createOrderBreakdown(orderItems) {
+    let orderBreakdownHTML = ''
+    for (const orderItem of orderItems) {
+        orderBreakdownHTML += `
+        <ul class='collapsible-details'>
+            <li><input type='checkbox' class='item-check'></li>
+            <li id='pastry'><img src='../global/images/cheesecake2.jpg'></li>
+            <li>${orderItem.getMenuItem().getName()}</li>
+            <li>${orderItem.getQty()}</li>
+            <li><img src='../global/icons/grayed_done_check.png' class='order-tick'></li>
+        </ul>`
+    }
+
     return orderBreakdownHTML;
 }
 
-function displayOrder(ordersDiv, orderIndex, customerUsername, menuItem, quantity){
-    let orderBreakdownHTML = '';
+
+function displayOrder(ordersDiv, orderIndex, customerUsername, orderItems) {
+    let orderBreakdownHTML = createOrderBreakdown(orderItems);
     let orderOverviewHTML = `
     <div class='order' id='order-no-${orderIndex}'>
         <div class='order-grid-wrap'>
@@ -130,7 +197,7 @@ function displayOrder(ordersDiv, orderIndex, customerUsername, menuItem, quantit
             </ul>
             <ul class='visible-order'>
                 <li><img src='../global/icons/grayed_checkmark.png'></li>
-                <li class='show-hide'><img src='../global/icons/expand_more-black-48dp.svg'></li>
+                <li class='show-hide'><img src='../global/icons/expand_more-black-48dp.svg' class='show-hide'></li>
             </ul>
         </div>
         <div class='collapsible-wrapper'>
@@ -140,11 +207,10 @@ function displayOrder(ordersDiv, orderIndex, customerUsername, menuItem, quantit
                 <li>Quantity</li>
             </ul>
             <hr>
-            ${orderBreakdownHTML};
+            ${orderBreakdownHTML}
         </div>
     </div>`;
-    // Temporarily making this an empty string
 
-    
+
     ordersDiv.innerHTML += orderOverviewHTML;
 }
--- persistence/db/insert_queries.js
+++ persistence/db/insert_queries.js
@@ -6,21 +6,21 @@ const { MongoClient, ObjectID } = require("mongodb");
 /**
  * Abstract class to define a MongoDb document
  */
-class InsertQuery{
-    constructor(){
+class InsertQuery {
+    constructor() {
         // Initialize the client and database objects
-        const uri =  `mongodb+srv://admin_COMP2140:${process.env.PASSWORD}@${process.env.CLUSTERURL}/?authMechanism=${process.env.AUTHMETH}`;
-        this.client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+        const uri = `mongodb+srv://admin_COMP2140:${process.env.PASSWORD}@${process.env.CLUSTERURL}/?authMechanism=${process.env.AUTHMETH}`;
+        this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     }
-}  
+}
 
-class CustomerInsertQuery extends InsertQuery{
+class CustomerInsertQuery extends InsertQuery {
     // Search Queries
-    constructor(){
+    constructor() {
         super();
     }
 
-    async insertOneCustomer(customerDocument){
+    async insertOneCustomer(customerDocument) {
         // Allow the client to connect before making query
         await this.client.connect();
         let userCollection = this.client.db('sweetb').collection("users");
@@ -29,7 +29,7 @@ class CustomerInsertQuery extends InsertQuery{
         this.client.close();
     }
 
-    async insertManyCustomers(customerDocuments){
+    async insertManyCustomers(customerDocuments) {
         // Ensure that the documents are inserted in order
         const options = { ordered: true };
         await this.client.connect();
@@ -37,40 +37,73 @@ class CustomerInsertQuery extends InsertQuery{
         const result = await userCollection.insertMany(customerDocuments, options);
         console.log(`${result.insertedCount} documents were inserted`);
     }
-}   
+}
 
 
-class OrderInsertQuery extends InsertQuery{
+class OrderInsertQuery extends InsertQuery {
     /**
      * Used for querying orders from the database
      * @param {String} dbPassword The password of the database
      */
-    constructor(){
+    constructor() {
         super();
-    }   
+    }
 
     /**
      * Inserts one order into the database
      * @param {object} document The JSON object of an order
      */
-    async insertOneOrder(document, databaseObj){
-        const collection = databaseObj.collection('orders');
-        let result = await collection.insertOne(document);
+    async insertOneOrder(orderDocument) {
+        await this.client.connect();
+        let orderCollection = this.client.db('sweetb').collection('orders');
+        let result = await orderCollection
+        .insertOne(orderDocument);
         console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`)
+        this.client.close();
     }
 
     /**
      * Inserts multiple orders into the database
      * @param {Array} documentsArr An array containing JSON objects representing an order
      */
-    async insertOrders(documentsArr, databaseObj){
+    async insertManyOrders(orderDocumentArr) {
         const options = { ordered: true };
-        const collection = databaseObj.collection('orders');
-        const result = await collection.insertMany(documentsArr, options);
+        await this.client.connect();
+        let orderCollection = this.client.db('sweetb').collection('orders');
+        const result = await orderCollection.insertMany(orderDocumentArr, options);
         console.log(`${result.insertedCount} documents were inserted`);
+        this.client.close();
     }
 }
 
+const testOrderDocument1 = {
+    _id: new ObjectID(),
+    customer: 'noelpo',
+    items: [{ qty: 5, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
+    { qty: 3, menuitem: new ObjectID('5fbd7beb98af411d70c0416b') },
+    { qty: 2, menuitem: new ObjectID('5fbd7beb98af411d70c0416b') },
+    ],
+}
+
+const testOrderDocument2 = {
+    _id: new ObjectID(),
+    customer: 'L0ki',
+    items: [{ qty: 10, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
+    { qty: 2, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
+    ],
+}
+
+const testOrderDocument3 = {
+    _id: new ObjectID(),
+    customer: 'Rick-Ashley',
+    items: [{ qty: 1, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') }
+    ],
+}
+
+const testOrders = [testOrderDocument1];
+
+let orderQuery = new OrderInsertQuery();
+//orderQuery.insertOneOrder(testOrderDocument3);
 /*
 const testCustomerObj = new Customer('BobAnderson420', 'test@bob.com', 'supersecretpass', null, null);
 console.log(testCustomerObj.getPassword());
@@ -86,4 +119,4 @@ let custQuery = new CustomerInsertQuery();
 custQuery.insertOneCustomer(testCustomerDocument);
  */
 
-module.exports = {OrderInsertQuery, CustomerInsertQuery};
+module.exports = { OrderInsertQuery, CustomerInsertQuery };
