f10bed616b0f0279debd6968368d36b73a9b15fb
DangaRanga <jasonblue1626@gmail.com>
2020-12-02 15:14:32 -0500

    Uncommented sessions and removed deprecated queries.js module

--- presentation/management_portal/order_collator.php
+++ presentation/management_portal/order_collator.php
@@ -4,10 +4,10 @@ session_start();
 include_once("../../aggregation/data_classes/user.php");
 $user = unserialize($_SESSION["user"]);
 ?>
+
 <?php if($user!=null && $user->getType()=="Admin"):?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -62,7 +62,6 @@ $user = unserialize($_SESSION["user"]);
 </body>
 
 </html>
-
 
 <?php endif;?>
 
