251f6b23af95bea20a063565e728b553c9c10ff5
richtwin567 <anakai.richards@gmail.com>
2020-12-02 14:57:00 -0500

    session fixes

--- presentation/management_portal/order_collator.php
+++ presentation/management_portal/order_collator.php
@@ -3,7 +3,7 @@ session_start();
 include_once("../../aggregation/data_classes/user.php");
 $user = unserialize($_SESSION["user"]);
 ?>
-<?php //if($user!=null && $user->getType()=="Admin"):?>
+<?php if($user!=null && $user->getType()=="Admin"):?>
 <!DOCTYPE html>
 <html>
 
@@ -63,12 +63,12 @@ $user = unserialize($_SESSION["user"]);
 </html>
 
 
-<?php //endif;?>
+<?php endif;?>
 
 <?php 
-/*
+
 if($user==null || $user->getType()=="Customer"){
     include("../global/templates/access_denied.php");
-}*/
+}
 
 ?>
