606f5b86f30224ad1b07332a4acfcb22ceaefb87
richtwin567 <anakai.richards@gmail.com>
2020-12-02 14:50:40 -0500

    linksadded to portal side bar

--- presentation/global/templates/portal_sidebar.php
+++ presentation/global/templates/portal_sidebar.php
@@ -1,22 +1,22 @@
 <div class='sidebar-wrapper'>
     <div class='sidebar'>
         <ul>
-            <li><a href=''>
+            <li><a href='dashboard.php'>
                 <div id='dashboard-icon'></div>
                 Dashboard
                 </a>
             </li>
-            <li><a href='#'>
+            <li><a href='order_collator.php'>
                 <div id='orders-icon'></div>
                 Orders
                 </a>
             </li>
-            <li><a href='#'>
+            <li><a href='shopping_list.php'>
                 <div id='shopping-list-icon'></div>
                 Shopping List
                 </a>
             </li>
-            <li><a href='#'>
+            <li><a href='menu_manager.php'>
                 <div id='menu-manager-icon'></div>
                 Menu Manager
                 </a>
