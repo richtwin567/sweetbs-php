c55924d156c0545e2e7e11d2b596c207ef60508c
chop <56415148+noelpo@users.noreply.github.com>
2020-12-02 08:51:09 -0600

    Added new  php file

--- presentation/management_portal/menu.php
+++ presentation/management_portal/menu.php
@@ -14,47 +14,116 @@ $user = $session->whoIsLoggedIn();
     <meta name="description" content="">
     <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./styles/main.css">
     
 </head>
 
 <body>
 
-<!--Top navigation-->
-
-<div class="topnav">
-      <div class="top-navleft">
-        <!--  <img src="https://www.kindpng.com/picc/m/276-2764257_instagram-logo-small-size-hd-png.png"> -->
-
+<div class="navbar">
+      <img src="logo.jpg" alt="logo">
+      <div class="right">
+        <a href="#"><i class="fa fa-bell"></i></a> 
+        <a href="#"><i class="fa fa-home"></i></a> 
+       <a href="#"><i class="fa fa-user-circle-o"></i></a> 
+        <a href="#"><i class="fa fa-power-off"></i></a>
       </div>
-      <div class="name">
-          <h6>Admin Portal</h6><br>
+    </div>
+
+    <div class="row">
+      <div class="leftcolumn">
+        <a href="#"><i class="fa fa-th-large" aria-hidden="true"></i> <span>Dashboard</span></a>
+        <a href="#"><i class="fa fa-users" aria-hidden="true"></i> <span>Orders</span></a>
+        <a href="#"><i class="fa fa-truck" aria-hidden="true"></i> <span>Shopping List</span></a>
+        <a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i> <span>Menu Manager</span></a>
       </div>
+      <div class="rightcolumn">
+        <h2>Menu Manager</h2>
 
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
-     
-     
-     <div class="topnav-right">
-         <a href="active"><i class="fa fa-bell"></i></a>
-         <a href="#home"><i class="fa fa-home"></i></a>
-         <a href="#user"><i class="fa fa-user-circle"></i></a>
-         <a href="#signout"><i class="fa fa-power-off"></i></a>
-     </div>
- </div>
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
-  <!--Side navigation-->
-  <div class="sidenav">
-      <ul type="disk">
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
-        <li><a href="#dashboard"><i class="fa fa-square"></i>Dashboard</a></li>
-        <li><a href="#orders"><i class="fa fa-users"></i>Orders</a></li>
-        <li><a href="#shoppinglist"><i class="fa fa-truck"></i> Shopping List</a></li>
-        <li><a href="menumanager.html"><i class="fa fa-bars"></i> Menu Manager</a></li>
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
-      </ul>
-  </div>
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
 
+        <div class="card">
+          <div class="card-top">
+            <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+          </div>
+          <img src="cheesecake1.jpg" alt="cheese cake" >
+          <p>Item name</p>
+          <p>Item Description</p>
+          <p><button>$###.##</button></p>
+        </div>
+        
+        <div class="card-plus">
+          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    
 
 
 
