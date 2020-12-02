0164f6df9f27cfc41731fdcde75ba3756b9e5d09
Jason <jasonblue1626@gmail.com>
2020-12-01 20:20:36 -0500

    Resolved merge conflict and removed test code

--- persistence/db/insert_queries.js
+++ persistence/db/insert_queries.js
@@ -53,17 +53,10 @@ class OrderInsertQuery extends InsertQuery {
      * Inserts one order into the database
      * @param {object} document The JSON object of an order
      */
-<<<<<<< HEAD
     async insertOneOrder(orderDocument) {
         await this.client.connect();
         let orderCollection = this.client.db('sweetb').collection('orders');
-        let result = await orderCollection
-        .insertOne(orderDocument);
-=======
-    async insertOneOrder(document){
-        const collection = this.client.db("sweetb").collection('orders');
-        let result = await collection.insertOne(document);
->>>>>>> 2069467065b9a3d9439d329b8543dd4eabecf2fb
+        let result = await orderCollection.insertOne(orderDocument);
         console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`)
         this.client.close();
     }
@@ -72,64 +65,14 @@ class OrderInsertQuery extends InsertQuery {
      * Inserts multiple orders into the database
      * @param {Array} documentsArr An array containing JSON objects representing an order
      */
-<<<<<<< HEAD
     async insertManyOrders(orderDocumentArr) {
         const options = { ordered: true };
         await this.client.connect();
         let orderCollection = this.client.db('sweetb').collection('orders');
         const result = await orderCollection.insertMany(orderDocumentArr, options);
-=======
-    async insertOrders(documentsArr){
-        const options = { ordered: true };
-        const collection = this.client.db("sweetb").collection('orders');
-        const result = await collection.insertMany(documentsArr, options);
->>>>>>> 2069467065b9a3d9439d329b8543dd4eabecf2fb
         console.log(`${result.insertedCount} documents were inserted`);
         this.client.close();
     }
 }
-
-const testOrderDocument1 = {
-    _id: new ObjectID(),
-    customer: 'noelpo',
-    items: [{ qty: 5, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
-    { qty: 3, menuitem: new ObjectID('5fbd7beb98af411d70c0416b') },
-    { qty: 2, menuitem: new ObjectID('5fbd7beb98af411d70c0416b') },
-    ],
-}
-
-const testOrderDocument2 = {
-    _id: new ObjectID(),
-    customer: 'L0ki',
-    items: [{ qty: 10, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
-    { qty: 2, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') },
-    ],
-}
-
-const testOrderDocument3 = {
-    _id: new ObjectID(),
-    customer: 'Rick-Ashley',
-    items: [{ qty: 1, menuitem: new ObjectID('5fbd7b6b98af411d70c0416a') }
-    ],
-}
-
-const testOrders = [testOrderDocument1];
-
-let orderQuery = new OrderInsertQuery();
-//orderQuery.insertOneOrder(testOrderDocument3);
-/*
-const testCustomerObj = new Customer('BobAnderson420', 'test@bob.com', 'supersecretpass', null, null);
-console.log(testCustomerObj.getPassword());
-const testCustomerDocument = {
-    _id: new ObjectID(),
-    username: testCustomerObj.getUsername(),
-    email: testCustomerObj.getEmail(),
-    password: testCustomerObj.getPassword(),
-    type: 'Customer'
-}
-
-let custQuery = new CustomerInsertQuery();
-custQuery.insertOneCustomer(testCustomerDocument);
- */
 
 module.exports = { OrderInsertQuery, CustomerInsertQuery };
