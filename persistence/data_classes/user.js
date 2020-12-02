d32a80efa2c432838078e6bd2e5776f4847789a7
richtwin567 <anakai.richards@gmail.com>
2020-12-01 20:13:56 -0500

    change exports to module.export in user.js

--- persistence/data_classes/user.js
+++ persistence/data_classes/user.js
@@ -1,112 +1,110 @@
 // Imports
 const ObjectID = require("mongodb").ObjectID;
-/** 
+/**
  *  This module defines the classes relating to the User class
- * @module user 
-*/
+ * @module user
+ */
 
 // Imports
 //const hashing = new (require('../security/hashing'));
 
-class User{
-    /**
-     * Constructor for initializing the User class
-     * @param {*} username 
-     * @param {*} email 
-     * @param {*} password 
-     */
-    constructor(username, email, password){
-        this.id = new ObjectID();
-        this.username = username;
-        this.email = email;
-        this.salt = hashing.generateSalt(20);
-        this.password = hashing.hashPassword(password, this.salt);
-    }
+class User {
+	/**
+	 * Constructor for initializing the User class
+	 * @param {*} username
+	 * @param {*} email
+	 * @param {*} password
+	 */
+	constructor(username, email, password) {
+		this.id = new ObjectID();
+		this.username = username;
+		this.email = email;
+		this.salt = hashing.generateSalt(20);
+		this.password = hashing.hashPassword(password, this.salt);
+	}
 
-    getUsername(){
-        return this.username;
-    }
+	getUsername() {
+		return this.username;
+	}
 
-    getEmail(){
-        return this.email;
-    }
+	getEmail() {
+		return this.email;
+	}
 
-    getPassword(){
-        return this.password;
-    }
+	getPassword() {
+		return this.password;
+	}
 
-    /**
-     * Sets the new username for the user
-     * @param {string} newUsername The new username
-     */
-    setUsername(newUsername){
-        this.username = newUsername;
-    }
+	/**
+	 * Sets the new username for the user
+	 * @param {string} newUsername The new username
+	 */
+	setUsername(newUsername) {
+		this.username = newUsername;
+	}
 
-    /**
-     * Sets the new email address for the user
-     * @param {string} newEmail The new email address
-     */
-    setEmail(newEmail){
-        this.email = newEmail;
-    }
+	/**
+	 * Sets the new email address for the user
+	 * @param {string} newEmail The new email address
+	 */
+	setEmail(newEmail) {
+		this.email = newEmail;
+	}
 
-    getSalt(){
-        return this.salt;
-    }
-    /**
-     * Sets the new password hash for the user's password and generates a new salt.
-     * @param {string} newPassword 
-     */
-    setPassword(newPassword){
-        // Generate a new salt
-        this.salt = hashing.generateSalt(20);
-        this.password = hashing.hashPassword(newPassword, this.salt);
-    }
+	getSalt() {
+		return this.salt;
+	}
+	/**
+	 * Sets the new password hash for the user's password and generates a new salt.
+	 * @param {string} newPassword
+	 */
+	setPassword(newPassword) {
+		// Generate a new salt
+		this.salt = hashing.generateSalt(20);
+		this.password = hashing.hashPassword(newPassword, this.salt);
+	}
 }
 
+class Customer extends User {
+	/**
+	 * Constructor for initializing the Customer class
+	 * @param {RealName} name The name of the customer
+	 * @param {Address} deliveryAddress The delivery address of the customer
+	 */
+	constructor(username, email, password, name, deliveryAddress) {
+		super(username, email, password);
+		this.name = name;
+		this.deliveryAddress = deliveryAddress;
 
-class Customer extends User{
-    /**
-     * Constructor for initializing the Customer class
-     * @param {RealName} name The name of the customer
-     * @param {Address} deliveryAddress The delivery address of the customer
-     */
-    constructor(username, email, password, name, deliveryAddress){
-        super(username, email, password);
-        this.name = name;
-        this.deliveryAddress = deliveryAddress;
-        
-        // TODO - Implement after the project
-        this.card = null;
-    }
+		// TODO - Implement after the project
+		this.card = null;
+	}
 
-    getRealName(){
-        return this.name;
-    }
+	getRealName() {
+		return this.name;
+	}
 
-    getDeliveryAddress(){
-        return this.deliveryAddress;
-    }
+	getDeliveryAddress() {
+		return this.deliveryAddress;
+	}
 
-    setRealName(newName){
-        this.name = newName;
-    }
+	setRealName(newName) {
+		this.name = newName;
+	}
 }
 
-class Admin extends User{
-    constructor(username, email, password){
-        super(username, email, password);
-    }
+class Admin extends User {
+	constructor(username, email, password) {
+		super(username, email, password);
+	}
 }
-
-
-
-
 
 // const testUser = new User('bobross','bob@gmail.com', 'Testo');
 
 // console.log(testUser.getPassword());
 
 // Exporting classes
-export {Customer, Admin};
+module.exports = {
+	Customer,
+	Admin,
+};
